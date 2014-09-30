52a12a9f6168324c41f80c5a610115bb7600d8c0
kyfxbl <kyfxbl@gmail.com>
2014-09-30 15:32:12 +0800

    1、不使用request，改用urlib。原因是request依赖的module太多，而且提交form file时有问题 2、拆分服务文件，更容易阅读 3、开始增加认证服务号的高级接口

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var immediate = require("./lib/immediately-response");
-var parser = require("./lib/express-weixin-bodyparser");
-var virgin = require("./lib/dev-validate");
-var fundamental = require("./lib/fundamental");
-var menu = require("./lib/menus");
+var passive = require("./lib/passive_message");
+var parser = require("./lib/wechat_bodyparser");
+var virgin = require("./lib/enable_dev_mode");
+var media = require("./lib/media");
+var menu = require("./lib/toolbar_menu");
+var cs = require("./lib/customer_service_message");
+var access_token = require("./lib/access_token");
+var validate = require("./lib/validate");
 
 // enable development mode at first time
 exports.enable_dev_mode = virgin.enable_dev_mode;
@@ -11,14 +14,18 @@ exports.enable_dev_mode = virgin.enable_dev_mode;
 exports.xml_parser = parser;
 
 // validate the message, to confirm it comes from wechat server
-exports.validate = immediate.validate;
+exports.validate = validate.validate;
 
 // reply message
-exports.replyTextMessage = immediate.replyTextMessage;
-exports.replyNewsMessage = immediate.replyNewsMessage;
+exports.replyTextMessage = passive.replyTextMessage;
+exports.replyNewsMessage = passive.replyNewsMessage;
 
-// fundamental
-exports.getAccessToken = fundamental.getAccessToken;
+// access_token
+exports.getAccessToken = access_token.getAccessToken;
 
 // custom menu
 exports.createMenu = menu.createMenu;
+
+// customer service message
+exports.csReplyText = cs.replyTextMessage;
+exports.csReplyNews = cs.replyNewsMessge;
